625a3705fa6e33f4aa55f17d3c2483450711b32f
koin1994 <rlaxoduq133@naver.com>
2022-09-21 17:33:51 +0900

    javaclass

--- ch06/src/sec2/Student.java
+++ ch06/src/sec2/Student.java
@@ -6,36 +6,42 @@ public class Student {
 	int eng;
 	int mat;
 	
-	public Student(){}
-	public Student(String name){}
-	public Student(String name, int kor, int eng, int mat){
-		this.name = name; this.kor = kor; 
-		this.eng = eng; this.mat = mat;
+	public Student() { }
+	public Student(String name){ this.name = name; }
+	public Student(String name, int kor, int eng, int mat) {
+		this.name = name;		this.kor = kor;
+		this.eng = eng;		this.mat = mat;
 	}
-	public String getName() {
-		return name;
+	
+	public int calcTot(){
+		int tot = kor+eng+mat; 
+		return tot;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public double calcAvg(){
+		double avg = (float) (Math.round((kor+eng+mat)/3*100)/100); 
+		return avg;
 	}
-	public int getKor() {
-		return kor;
+	public String calHak(){
+		String hak="";
+		double avg = this.calcAvg();
+		if(avg>=90) {
+			hak="A";
+		} else if(avg>=80){
+			hak="B";
+		} else if(avg>=70){
+			hak="C";
+		} else {
+			hak="D";
+		}
+		return hak;
 	}
-	public void setKor(int kor) {
-		this.kor = kor;
+	public String calcRes(){
+		double avg = this.calcAvg();
+		String res = "불합격";
+		if(avg>=80) res="합격";
+		return res;
 	}
-	public int getEng() {
-		return eng;
-	}
-	public void setEng(int eng) {
-		this.eng = eng;
-	}
-	public int getMat() {
-		return mat;
-	}
-	public void setMat(int mat) {
-		this.mat = mat;
-	}
-
-	 
 }
+		
+	
+
